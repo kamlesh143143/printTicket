6b31a0ccba28b6f6c9c86b4dffcef18b1ba61092
Kamlesh Thavani <32490354+kamlesh143143@users.noreply.github.com>
2019-12-25 00:12:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const fs = require("fs");
 const { createCanvas, Image  } = require("canvas");
+const fs = require("fs");
+
+function addText(context, fSize,text,x,y) {
+    context.font = fSize+"px Arial";
+    context.fillText(text, x, y);
+}
+
 
 const WIDTH = 360;
 const HEIGHT = 600;
@@ -10,13 +16,6 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-
-function addText(context, fSize,text,x,y) {
-    context.font = fSize+"px Arial";
-    context.fillText(text, x, y);
-}
-
-// From a local file path:
 const img = new Image()
 img.onload = () => ctx.drawImage(img, 30,10,300,70)
 img.onerror = err => { throw err }
@@ -24,10 +23,8 @@ img.src = './Desert.jpg'
 
 ctx.fillStyle = "#000000";
 
-
-addText(ctx, 18, "Hello World", 90, 200);
-addText(ctx, 19, "Hello World", 90, 220);
-
+addText(ctx, 18, "Hello World 1", 90, 200);
+addText(ctx, 19, "Hello World 2", 90, 220);
 
 const buffer = canvas.toBuffer("image/jpeg");
 fs.writeFileSync("test.jpg", buffer);
