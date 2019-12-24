d6705064f3c95489c4b3f014dac858fc24e993fd
Kamlesh Thavani <32490354+kamlesh143143@users.noreply.github.com>
2019-12-24 23:56:30 +0530

    Update index.js
    function add

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const fs = require("fs");
 const { createCanvas, Image  } = require("canvas");
 
-const WIDTH = 300;
+const WIDTH = 360;
 const HEIGHT = 600;
 
 const canvas = createCanvas(WIDTH, HEIGHT);
@@ -12,18 +11,23 @@ ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
 
+function addText(context, fSize,text,x,y) {
+    context.font = fSize+"px Arial";
+    context.fillText(text, x, y);
+}
+
 // From a local file path:
 const img = new Image()
-img.onload = () => ctx.drawImage(img, 40,10,100,100)
+img.onload = () => ctx.drawImage(img, 30,10,300,70)
 img.onerror = err => { throw err }
 img.src = './Desert.jpg'
 
 ctx.fillStyle = "#000000";
-ctx.font = "18px Arial";
-ctx.fillText("Hello", 90, 140);
 
-// ctx.font = "22px Arial";
-// ctx.fillText("Hello", 13, 55);
 
-const buffer = canvas.toBuffer("image/png");
-fs.writeFileSync("test.png", buffer);
+addText(ctx, 18, "Hello World", 90, 200);
+addText(ctx, 19, "Hello World", 90, 220);
+
+
+const buffer = canvas.toBuffer("image/jpeg");
+fs.writeFileSync("test.jpg", buffer);
